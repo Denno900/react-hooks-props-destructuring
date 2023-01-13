0ea61da23401b60359d9153c33ff079041cb5ce9
dennis mwaniki <dennomaz@gmail.com>
2023-01-13 15:23:09 +0300

    assignment complete

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function MovieCard({
   title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
+  posterSrc = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSJkc5QBoCplF1PKcJyX4Ag0tfDuoXBbolfkYDfCYGGaeYNpXtc",
   genres,
 }) {
   return (
